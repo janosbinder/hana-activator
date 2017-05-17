8cc9fee8613512bbafa022eb3d9e5c080fc50c76
Janos Binder <janos.binder@molecularhealth.com>
2017-05-17 17:21:04 +0200

    Added delete and activate methods to HanaActivationClient

--- src/main/scala/HanaActivationClient.scala
+++ src/main/scala/HanaActivationClient.scala
@@ -1,6 +1,40 @@
+import org.apache.http.HttpResponse
+import org.apache.http.client.methods.{HttpDelete, HttpPut}
+
 /**
   * Created by jbinder on 16.05.17.
   */
-class HanaActivationClient(hanaServer:String, hanaPort:Int, hanaUser:String, hanaPassword:String) extends HanaClient(hanaServer, hanaPort, hanaUser, hanaPassword){
+class HanaActivationClient(hanaServer: String, hanaPort: Int, hanaUser: String, hanaPassword: String) extends HanaClient(hanaServer, hanaPort, hanaUser, hanaPassword) {
+  def getRoute(route: String): String = {
+    // See /sap/hana/xs/dt/base/.xsaccess for the routes
+    val routes = Map(
+      "FILE" -> "/file",
+      "WORKSPACE" -> "/workspace",
+      "IMPORT" -> "/xfer/import",
+      "EXPORT" -> "/xfer/export",
+      "METADATA" -> "/metadata",
+      "CHANGE" -> "/change",
+      "INFO" -> "/info")
 
+    routes.get(route) match {
+      case Some(r) => return s"http://$hanaServer:$hanaPort/sap/hana/xs/dt/base$r"
+      case None => throw new HanaClientException(s"$route does not exists")
+    }
+  }
+
+  def delete(path: String): HttpResponse = {
+    val deleteCall = new HttpDelete(getRoute("FILE") + "/" + path)
+    val response = super.executeRequest(deleteCall)
+    return response
+  }
+
+  def activate(path: String): HttpResponse = {
+    val activateCall = new HttpPut(getRoute("FILE") + "/" + path + "?parts=meta")
+    activateCall.addHeader("X-Requested-With", "XMLHttpRequest")
+    activateCall.addHeader("Content-Type", "application/json")
+    activateCall.addHeader("SapBackPack", "{\"MassTransfer\":true, \"Activate\":true}")
+    val response = super.executeRequest(activateCall)
+    return response
+
+  }
 }
--- src/main/scala/HanaClient.scala
+++ src/main/scala/HanaClient.scala
@@ -5,26 +5,26 @@ import java.nio.charset.StandardCharsets
 import org.apache.http.HttpResponse
 import org.apache.http.client.HttpClient
 import org.apache.http.client.config.RequestConfig
-import org.apache.http.client.methods.{HttpHead, HttpPost}
+import org.apache.http.client.methods.{HttpHead, HttpPost, HttpUriRequest}
 import org.apache.http.client.protocol.HttpClientContext
 import org.apache.http.impl.client.{BasicCookieStore, CloseableHttpClient, HttpClients}
 
 /**
   * Created by jbinder on 16.05.17.
   */
-class HanaClient(val hanaServer:String, val hanaPort:Int, val hanaUser:String, val hanaPassword:String) extends Closeable {
-  val client : CloseableHttpClient = createClient
-  val response : HttpResponse= fetchXCSRFToken(client)
+class HanaClient(val hanaServer: String, val hanaPort: Int, val hanaUser: String, val hanaPassword: String) extends Closeable {
+  private val client: CloseableHttpClient = createClient
+  val response: HttpResponse = fetchXCSRFToken(client)
 
-  val cookies : String = response.getHeaders("set-cookie").map(h => h.getValue).mkString(";")
-  val token : String = response.getHeaders("x-csrf-token").map(h => h.getValue).mkString(";")
-  if( cookies == "" || token == "") {
-      throw new HanaClientException(s"X-CSRF-Token token receipt failed - Check authentication at $hanaServer:$hanaPort with user $hanaUser")
+  val cookies: String = response.getHeaders("set-cookie").map(h => h.getValue).mkString(";")
+  val token: String = response.getHeaders("x-csrf-token").map(h => h.getValue).mkString(";")
+  if (cookies == "" || token == "") {
+    throw new HanaClientException(s"X-CSRF-Token or cookie receipt failed - Check authentication at $hanaServer:$hanaPort with user $hanaUser")
   }
 
   override def close(): Unit = {
     if (token != "") {
-      val logoutEndpoint = s"$hanaServer:$hanaPort/sap/hana/xs/formLogin/logout.xscfunc"
+      val logoutEndpoint = s"http://$hanaServer:$hanaPort/sap/hana/xs/formLogin/logout.xscfunc"
       val logoutCall = new HttpPost(logoutEndpoint)
       logoutCall.addHeader("X-CSRF-Token", token)
       client.execute(logoutCall)
@@ -32,11 +32,22 @@ class HanaClient(val hanaServer:String, val hanaPort:Int, val hanaUser:String, v
     client.close()
   }
 
-  private def fetchXCSRFToken(client : HttpClient) : HttpResponse = {
+  def executeRequest(request: HttpUriRequest): HttpResponse = {
+    if (token == "") {
+      throw new HanaClientException(s"X-CSRF-Token is missing - Check authentication at $hanaServer:$hanaPort with user $hanaUser")
+    }
+    val headers = Map("X-CSRF-Token" -> token,
+      "Cookie" -> cookies,
+      "Orion-Version" -> "1.0")
+    headers.foreach { case (k, v) => request.addHeader(k, v) }
+    return client.execute(request)
+  }
+
+  private def fetchXCSRFToken(client: HttpClient): HttpResponse = {
     val authenticationHeader = Base64.getEncoder.encodeToString(s"$hanaUser:$hanaPassword".getBytes(StandardCharsets.UTF_8))
     val headers = Map("X-CSRF-Token" -> "Fetch",
-                      "X-Requested-With" -> "XMLHttpRequest",
-                      "Authorization" -> s"Basic $authenticationHeader")
+      "X-Requested-With" -> "XMLHttpRequest",
+      "Authorization" -> s"Basic $authenticationHeader")
     val authenticationEndpoint =
       s"http://$hanaServer:$hanaPort/sap/hana/xs/formLogin/token.xsjs"
     val xscrfCall = new HttpHead(authenticationEndpoint)
@@ -44,7 +55,7 @@ class HanaClient(val hanaServer:String, val hanaPort:Int, val hanaUser:String, v
     client.execute(xscrfCall)
   }
 
-  private def createClient : CloseableHttpClient = {
+  private def createClient: CloseableHttpClient = {
     val globalConfig = RequestConfig.custom.setCookieSpec("default").build
     val cookieStore = new BasicCookieStore
     val context = HttpClientContext.create
