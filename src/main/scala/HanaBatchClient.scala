a63e3ddc4c8d572d4716ad873d0a2d334eab831b
Janos Binder <janosbinder@hotmail.com>
2017-07-04 22:41:16 +0200

    Added stub of Batch client

---
+++ src/main/scala/HanaBatchClient.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by jbinder on 04.07.17.
+  */
+class HanaBatchClient(protocol: String, host: String, port: Int, user: String, password: String, dest: String) extends HanaActivationClient(protocol, host, port, user, password, dest) {
+
+}
